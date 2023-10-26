11b2178c39ca7320804db8b22b8fff87d42b5deb
Radomir Gross <grossbrn1990@gmail.com>
2023-10-26 17:23:11 +0700

    added more methods

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,15 +1,17 @@
   package org.example;
 
+  import java.util.Iterator;
+  import java.util.NoSuchElementException;
   import java.util.function.Function;
 
 
-    public class MyList<T extends Number> {
+    public class MyList<T extends Number> implements Iterable{
       private final int STARTING_CAPACITY=10;
       private Object[] data;
       private int size;
 
       public MyList() {
-        this.data = new Object[STARTING_CAPACITY]; // начальная емкость
+        this.data = new Object[STARTING_CAPACITY];
         this.size = 0;
       }
       public void add(T element) {
@@ -18,23 +20,75 @@
         }
         data[size++] = element;
       }
-    public Object get(int index) {
-      throw new RuntimeException("Not implemented");
+    public T get(int index) {
+        if (index<0||index>=size)
+        {IndexOutOfBoundsException exception=new IndexOutOfBoundsException();
+          exception.printStackTrace();
+          throw exception;}
+      return (T)data[index];
     }
 
     private void resize() {
-      throw new RuntimeException("Not implemented");
+
+       Object[] newdata=new Object[size*2];
+       System.arraycopy(data,0,newdata,0,data.length);
+       data=newdata;
     }
 
-    public Object remove(int index) {
-      throw new RuntimeException("Not implemented");
+    public T remove(int index) {
+     if (index<0||index>=data.length)
+     {IndexOutOfBoundsException exception=new IndexOutOfBoundsException();
+     exception.printStackTrace();
+     throw exception;}
+     T removed=(T) data[index];
+     System.arraycopy(data,index++,data,index,size-index-1);
+     data[size--]=null;
+     return removed;
     }
 
-    public MyList map(Function f) {
-      throw new RuntimeException("Not implemented");
-    }
+
+        public <R extends Number>MyList<R> map(Function<T, R> f) {
+            MyList<R> newMyList = new MyList<>();
+            for (int i = 0; i < size; i++) {
+                newMyList.add( f.apply(this.get(i)));
+            }
+            return newMyList;
+        }
 
     public int size() {
-      throw new RuntimeException("Not implemented");
+      return size;
     }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new MyListIterator();
+        }
+
+        private class MyListIterator implements Iterator<T>{
+          private int currentIndex=0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex<size;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext())
+                    return (T)data[currentIndex++];else
+                        throw new NoSuchElementException();
+
+            }
+        }
+
+        public static void main(String[] args) {
+            MyList<Integer> list=new MyList<>();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            MyList<Integer> newList = list.map(x -> x * 1000);
+            for (Object x:newList) {
+                System.out.println(x);
+            }
+        }
   }
